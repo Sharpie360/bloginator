d105a63cff700ffe54876ec892949d5290cc258d
SeanBecker <techtronixwebdesign@gmail.com>
2018-09-18 00:11:50 -0500

    replaced most vanilla js with VueJS

--- js/get-posts.js
+++ js/get-posts.js
@@ -10,51 +10,5 @@ fetch(`https://www.googleapis.com/blogger/v3/blogs/${blogId}/posts?key=${API_KEY
 .then(response => response.json())
 .then(data => {
   posts.unshift(...data.items)
-  loadPosts();
-});
-
-function loadPosts(){
   vm.$data.body = posts[0].content;
-  let list = '';
-  posts.forEach(post => {
-    getSubtitle(post.content)
-    list += `
-    <li class="post-list-item">
-      <a href="#" data-postID="${post.id}" class="pl-link">
-        <h3 class="pl-item-title">${post.title}</h3>
-        <span class="pl-item-subtitle">${subtitle}</span>
-      </a>
-    </li>`
-  })
-  postList.innerHTML = list;
-}
-
-
-function getSubtitle(post){
-  if(post == ''){
-    console.log('no post available, please check your code and try again.')
-  } else {
-    const text = post;
-    // closing </h1> tag
-    const stringToMatch = '(<\\/h1>)';
-    // searches post content for closing h1 tag
-    const regex = new RegExp(stringToMatch, ["i"]);
-    const match = text.split(regex)
-    if (match != null) {
-      // console.log(match)
-      subtitleString = match.splice(0, 2).join('')
-      // create ghost dom element
-      let ghost = document.createElement('span')
-      //set ghost html to 
-      ghost.innerHTML = subtitleString
-      subtitle = ghost.textContent
-      return subtitle
-    } else {
-      subtitle = 'No Subtitle Available'
-    }
-  }
-}
-
-
-
-// setTimeout(() => {getSubtitle(posts[0].content)}, 500)
+});
--- js/post-list.js
+++ js/post-list.js
@@ -1,28 +1,36 @@
-// vanillaJS script
-postList.addEventListener('click', function(e){
-  console.log(e.target)
-  if (e.target.parentElement.classList.contains('pl-link')){
 
-    targetBlogID = e.target.parentElement.dataset.postid;
-    console.log(targetBlogID)
-    // loop thru posts and find matching id
-    for (let i = 0; i < posts.length; i++){
-      if (targetBlogID === posts[i].id){
-        newContent = posts[i].content;
-        console.log(newContent)
-        // vanilla js
-        // blogPostDisplay.innerHTML = posts[i].content;
-
-        //vue.js
-        vm.$data.body = posts[i].content;
+vm_postList = new Vue({
+  el: '#sidebar',
+  data: {
+    posts: posts
+  },
+  methods: {
+    getSubs: function(post){
+      if(post == ''){
+        console.log('no post available, please check your code and try again.')
+      } else {
+        const text = post.content;
+        // closing </h1> tag
+        const stringToMatch = '(<\\/h1>)';
+        // searches post content for closing h1 tag
+        const regex = new RegExp(stringToMatch, ["i"]);
+        const match = text.split(regex)
+        if (match != null) {
+          // console.log(match)
+          subtitleString = match.splice(0, 2).join('')
+          // create ghost dom element
+          let ghost = document.createElement('span')
+          //set ghost html to 
+          ghost.innerHTML = subtitleString
+          subtitle = ghost.textContent
+          return subtitle
+        } else {
+          subtitle = 'No Subtitle Available'
+        }
       }
+    },
+    setContent: function(post){
+      vm.$data.body = post.content
     }
   }
 })
-
-// vm_postList = new Vue({
-//   el: '#sidebar',
-//   data: {
-//     posts: posts
-//   }
-// })
