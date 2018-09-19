b88aa981ef4d7d3cf607ad568ab2b93c169566e6
SeanBecker <techtronixwebdesign@gmail.com>
2018-09-19 01:54:07 -0500

    added - methods for createPost : saveDraft, commitPost, cancelPost | updated - index.html event listeners

--- js/post-content.js
+++ js/post-content.js
@@ -16,5 +16,30 @@ const vm_postCreate = new Vue({
     subtitle: '',
     body: '',
     createMode: vm_postDisplay.$data.createMode
+  },
+  methods: {
+    saveDraft: function(){
+      console.log('feature coming soon!')
+      postData = {
+        title: this.title,
+        subtitle: this.subtitle,
+        body: this.body
+      }
+      localStorage.setItem('postData', postData)
+      // gets it to object Object in LS
+      // coming back to this
+    },
+    commitPost: function(){
+      console.log('feature coming soon!')
+    },
+    cancelPost: function(){
+      if(confirm('Are you sure you want to cancel?')){
+        // set state to displayMode
+        vm_postDisplay.$data.createMode = false
+        console.log('leaving state: createMode |>>>| entering state: displayMode')
+      } else {
+        return
+      }
+    }
   }
 })
