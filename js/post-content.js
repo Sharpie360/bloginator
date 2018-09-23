514b083911be25a231cc33b53a6dc0ae08560bd1
SeanBecker <techtronixwebdesign@gmail.com>
2018-09-23 00:37:33 -0500

    finished - saveDraft, loadDraft functionality, updated html for load btn

--- js/post-content.js
+++ js/post-content.js
@@ -3,11 +3,11 @@ const vm_postDisplay = new Vue({
   el: '#view-post',
   data: {
     id: '',
-    title: '',
+    title: 'Welcome to Bloginator',
     subtitle: '',
     date: '',
-    body: '',
-    viewMode: true
+    body: 'Please sign in to continue...',
+    viewMode: true // refactor to global state in V2
   }
 })
 
@@ -17,22 +17,44 @@ const vm_postCreate = new Vue({
     title: '',
     subtitle: '',
     body: '',
-    createMode: false,
-    editMode: false
+    createMode: false, // refactor to global state in V2
+    editMode: false, // refactor to global state in V2
+    draftSaved: false
+
   },
   methods: {
     saveDraft: function(){
-      console.log('feature coming soon!')
       postData = {
         title: this.title,
         subtitle: this.subtitle,
         body: this.body
       }
-      localStorage.setItem('postData', postData)
-      // gets it to object Object in LS
-      // coming back to this
+      if(postData.title || postData.body == ''){
+        alert('please fill out at least the title and body')
+        return
+      }
+      if(localStorage.getItem('savedDraftData')){
+        preview = JSON.parse(localStorage.getItem('savedDraftData'))
+        if(confirm(`Overwrite last saved draft? Title: ${preview.title} | Body: ${preview.body}`)){
+          savedDraft_LS = JSON.stringify(postData)
+          localStorage.setItem('savedDraftData', savedDraft_LS)
+        } else {
+          return
+        }
+      } else {
+        savedDraft_LS = JSON.stringify(postData)
+        localStorage.setItem('savedDraftData', savedDraft_LS)
+      }
+    },
+    loadDraft: function(){
+      let loadedDraft_LS = localStorage.getItem('savedDraftData')
+      formattedDraft = JSON.parse(loadedDraft_LS)
+      vm_postCreate.$data.title = formattedDraft.title
+      vm_postCreate.$data.subtitle = formattedDraft.subtitle
+      vm_postCreate.$data.body = formattedDraft.body
     },
     commitPost: function(){
+      // check that all fields are filled out
       postData = {
         title: this.title,
         content: hackifyPostBody(this.subtitle, this.body)
