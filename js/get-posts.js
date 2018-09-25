1adf3e481bfcbe8f7b64230b5f44c313f9cee3dc
SeanBecker <techtronixwebdesign@gmail.com>
2018-09-25 16:55:56 -0500

    updated - added opacity 0 to post-list before get request : loads list in smoothly each request

--- js/get-posts.js
+++ js/get-posts.js
@@ -3,6 +3,7 @@
 // using let instead of const to be able to empty and fill the array as needed. YES. it is mutable. for this app its acceptable
 // let posts = []
 function getPosts(){
+  document.getElementById('post-list').style.opacity = 0
   fetch(`https://www.googleapis.com/blogger/v3/blogs/${blogId}/posts?key=${API_KEY}`)
   .then(response => response.json())
   .then(data => {
